6a0e6a97970e9c340d1111423c6f8f72afd7574d
Emma Velazquez <emmasiren24@gmail.com>
2022-12-08 22:30:00 -0800

    Rewrote src folder to get a working app to work off of.

---
+++ client/src/components/pages/CreateCharacter.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './createCharacter.css';
+
+export default function CreateCharacter() {
+  return (
+    <div>
+      <h1 className="title">Create Character</h1>
+        <form className="form">
+          <p>Name:</p>
+          <input
+            name="name"
+            type="text"
+          />
+        </form>
+        <button className="button" type="button">Submit</button>
+    </div>
+  );
+}
---
+++ client/src/components/pages/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Dashboard() {
+  return (
+    <div className="dashboard">
+      <h1 className="title">Dashboard</h1>
+      <div id="dashboard">
+        <p>
+          Lorem ipsum dolor
+        </p>
+       </div>
+    </div>
+  );
+}
