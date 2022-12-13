2c1c60aa32ce62f475fe31824cd2ed33e4fd01e7
AnthonyMartinez <anthonymartienz0423@gmail.com>
2022-12-12 21:25:42 -0800

    auth.js

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,89 +1,47 @@
-import React, { useState } from 'react;'
-import { Form, Button, Alert } from 'react-bootstrap';
-
-import { newUser } from '../utils/API';
+import decode from 'jwt-decode';
 
 
+class AuthService {
+    // get user data
+    getProfile() {
+        return decode(this.getToken());
+    }
 
-const Signup = () => {
-    const [userFormData, setUserFormData] =
-        useState({ username: '', email: '', password: '' });
+    loggedIn() {
 
-    const [validated] = useState(false);
-    const [showAlert, setShowAlert] = useState
-        (false);
+        const token = this.getToken();
+        return !!token && !this.isTokenExpired(token);
+    }
 
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setUserFormData({ ...userFormData, [name]: value });
-    };
 
-    const handleSumbit = async (event) => {
-        event.preventDefault();
-
-        const form = event.currentTarget;
-        if (form.checkValid() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-
-        }
+    isTokenExpired(token) {
         try {
-            const response = await newUser(userFormData);
-            if (!response.ok) {
-                throw new Error('Wrong!');
-            }
-            const { user } = await response.json();
-            console.log(user);
+            const decoded = decode(token);
+            if (decoded.exp < Date.now() / 1000) {
+                return true;
+            } else return false;
         } catch (err) {
-            console.error(err);
-            setShowAlert(true);
+            return false;
         }
-        setUserFormData({
-            username: '',
-            email: '',
-            password: '',
-        });
-    };
+    }
 
-    return (
-        <>
-            <Form noValidate validated={validated} onSubmit={handleSumbit}>
-                <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>Something went wrong with your signup</Alert>
-                <Form.Group>
-                    <Form.Label hmtlFor='username'>Username</Form.Label>
-                    <Form.Control
-                        type='text'
-                        placeholder='Your Username'
-                        name='username'
-                        onChange={handleInputChange}
-                        value={userFormData.username}
-                        required
-                    />
-                    <Form.Control.Feedback type='invalid'>Username is required</Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label htmlFor='email'>Email</Form.Label>
-                    <Form.Control
-                        type='email'
-                        placeholder='Eamil Address'
-                        name='email'
-                        onChange={handleInputChange}
-                        value={userFormData.email}
-                        required
-                    />
-                    <Form.Control.Feedback type='invalid'>Password is required</Form.Control.Feedback>
+    getToken() {
 
-                </Form.Group>
-                <Button
-                    disabled={!(userFormData.username &&
-                        userFormData.email && userFormData.password)}
-                    type='sumbit'
-                    variant='Access Granted'>
-                    Submit
-                </Button>
-            </Form>
-        </>
-    );
-};
+        return localStorage.getItem('id_token');
+    }
 
-export default Signup;
+    login(idToken) {
+
+        localStorage.setItem('id_token', idToken);
+        window.location.assign('/');
+    }
+
+    logout() {
+
+        localStorage.removeItem('id_token');
+
+        window.location.assign('/');
+    }
+}
+
+export default new AuthService();
