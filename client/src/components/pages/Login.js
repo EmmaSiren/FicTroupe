0e8fc83448a7bd908f27dc203f246e494938e84e
JiongW <jiongw22@gmail.com>
2022-12-15 00:08:31 -0800

    added login logic, need to fix the clear value re-rendering issue, and back end login logic

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,24 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Button, Form, Input, Row } from 'antd';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../../utils/mutations';
+import Auth from '../../utils/auth';
 
-export default function Login () {
+export default function Login (props) {
+  const [formState, setFormState] = useState({ username: '', password: '' });
+  const[login, { error, data }] = useMutation(LOGIN_USER);
+
+  // Update state based on input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    })
+  };
+
+  // Submit form
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // Clear the existing values
+  // setFormState({
+  //   email: '',
+  //   password: '',
+  // });
+
   return (
       <Row id="test2" justify="space-around">
       <Card style={{ width: 300, paddingTop: '24px'}} align="middle">
-        <Form name="normal_login" className="login-form" >
+        <Form name="normal_login" className="login-form">
           <Form.Item name="username" rules={[{ required: true, message: 'You forgot your username!'}]}>
-            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} 
+            placeholder="Username" 
+            name="username"
+            value={formState.username}
+            onChange={handleChange}
+             />
           </Form.Item>
           <Form.Item name="password" rules={[{ required: true, message: 'You forgot your password!'}]}>
-            <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="Password"/>          </Form.Item>
+            <Input prefix={<LockOutlined className="site-form-item-icon" />} 
+            type="password" 
+            placeholder="Password"
+            name="password"
+            value={formState.password}
+            onChange={handleChange}
+            />          
+          </Form.Item>
           <Form.Item>
-            <Button type="primary" htmlType="submit" className="login-form-button">
+            <Button type="primary" htmlType="submit" className="login-form-button" onClick={handleSubmit}>
               Log in
             </Button>
           </Form.Item>
           <Form.Item>
-              Or <a href="">Sign Up!</a>
+              Or <a href="./signup">Sign Up!</a>
           </Form.Item>     
         </Form>
       </Card>
--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -18,6 +18,8 @@ class AuthService {
         try {
             const decoded = decode(token);
             if (decoded.exp < Date.now() / 1000) {
+                // Need to add the following code?
+                // localStorate.removeItem('id_token')
                 return true;
             } else return false;
         } catch (err) {
@@ -26,20 +28,16 @@ class AuthService {
     }
 
     getToken() {
-
         return localStorage.getItem('id_token');
     }
 
     login(idToken) {
-
         localStorage.setItem('id_token', idToken);
         window.location.assign('/');
     }
 
     logout() {
-
         localStorage.removeItem('id_token');
-
         window.location.assign('/');
     }
 }
