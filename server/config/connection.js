bc65e121b71e50d976b098733d653699d5d2901d
Emma Velazquez <emmasiren24@gmail.com>
2022-12-19 19:23:43 -0800

    Attempting to deploy correctly

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://27017', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
