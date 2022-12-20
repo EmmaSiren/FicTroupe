2534c42bc57c3bbc7aff0e39a08288402c828fa3
Emma Velazquez <emmasiren24@gmail.com>
2022-12-19 21:00:34 -0800

    fixed cards

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -3,45 +3,21 @@ import CreateCharacter from './CreateCharacter';
 import { SmileOutlined, UserOutlined } from '@ant-design/icons';
 import {QUERY_USER} from '../../utils/queries'
 // import {QUERY_CHARACTERS} from '../../utils/queries'
-import { Card, Descriptions } from 'antd';
+import { Card, Descriptions, Col, Row } from 'antd';
 
 
-import { useQuery } from '@apollo/client';
-
-
-// export default function Dashboard({ character }) {
-//   return (
-//     <div className="container">
-//       <h1 className="title">Dashboard</h1>
-//       <h2>Your Characters</h2>
-//       <div className="row">
-//         {characters.map(character => (
-//           <div className="col" key={character.id}>
-//             <h3>{character.name}</h3>
-//             <img className="image" alt="" src={character.img} />
-            
+import { useQuery } from '@apollo/client';       
 
 export default function Dashboard() {
 
   const { loading, data } = useQuery(QUERY_USER)
   const characterData = data?.user || [];
-  // console.log("HERE"+characterData.user.myCharacters)
-  // console.log(data)
-  // console.log(data.user.myCharacters)
-  // const characterData =  data.user.myCharacters
   console.log(characterData)
   console.log(data)
-  // console.log(characterData.myCharacters)
 
   const displayCharacters = characterData.myCharacters;
 
   console.log(displayCharacters)
-
-
-
-
-  // const characters=data.myCharacters
-  // .user.myCharacters
   
   if (loading) {
     return <h2  className="testingbg" style={{ height: '98vh'}} id="loading">Loading...</h2>;
@@ -52,26 +28,21 @@ export default function Dashboard() {
       <h2 style={{ textAlign: 'center', color: '#211534' }}>Your Characters <SmileOutlined /></h2>
       <CreateCharacter className="handlee"/>
       
-        
-      {displayCharacters.map(character => (
-        <Card size="small" title={character.name} style={{ textAlign: 'center', background:'#FFDAD1' }}>
-  <div key={character._id}>
-    <h3><UserOutlined /> {character.name}</h3>
-    <h3><UserOutlined /> {character.description}</h3>
-    <h3><UserOutlined /> {character.universe}</h3>
-    <h3><UserOutlined /> {character.status}</h3>
-
-
-
-
-    {/* <h3><UserOutlined /> {character.author}</h3> */}
-
-    {/* <img style={{width: '100px'}} alt="" src={character.img} /> */}
-
-  </div>
-  </Card>
-))}
-
+      <Row gutter={16} justify="center">
+        {displayCharacters.map(character => (
+          <Col span={8} style={{margin: "20px"}}>
+          <Card title={character.name} bordered={false} style={{ textAlign: 'center', background:'#FFDAD1' }}>
+            <div key={character._id} style={{textAlign: "left"}}>
+              <h3>{character.description}</h3>
+              <h3>{character.universe}</h3>
+              <h3>{character.status}</h3>
+          {/* <h3>{character.author}</h3> */}
+          {/* <img style={{width: '100px'}} alt="" src={character.img} /> */}
+            </div>
+          </Card>
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 }
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -4,7 +4,7 @@ import { QUERY_CHARACTERS } from '../../utils/queries'
 import { useQuery } from '@apollo/client';
 
 import { TeamOutlined, UserOutlined } from '@ant-design/icons';
-import { Card, Descriptions } from 'antd';
+import { Card, Descriptions, Col, Row } from 'antd';
 import CreateCharacter from './CreateCharacter';
 
 export default function Home() {
@@ -12,21 +12,16 @@ export default function Home() {
   const { loading, data } = useQuery(QUERY_CHARACTERS)
   console.log(data)
   const characterData = data?.characters || [];
-  // console.log("HERE"+characterData)
-
   console.log(characterData)
 
   for (var i=0; i < characterData.length;i++){
     console.log(characterData.name)
   }
-var cname = []
-var cauth = []
-var cuni = []
-var cdest = []
-var cstat = []
-
-
-
+    var cname = []
+    var cauth = []
+    var cuni = []
+    var cdest = []
+    var cstat = []
 
   characterData.forEach(element => {
     console.log("Test")
@@ -41,45 +36,29 @@ var cstat = []
 
   console.log (cname)
 
-  // const displayCharacters = characterData.characters;
-  // console.log(displayCharacters)
-
-  // console.log(characterData)
-
   if (loading) {
     return <h2 className="testingbg" style={{ height: '98vh'}} id="loading">Loading...</h2>;
 
   }
   return (
-    <div>  
       <div className="testingbg" style={{ height: '98vh'}}>
         <h2 className="title">Home</h2>
-        <h2 style={{ textAlign: 'center' }}>New Characters <TeamOutlined />
-        </h2>
+        <h2 style={{ textAlign: 'center' }}>New Characters <TeamOutlined /></h2>
+        
+        <Row gutter={16} justify="center">
         {characterData.map(character => (
-        <Card size="small" title={character.name} style={{ textAlign: 'center', background:'#FFDAD1' }}>
-  <div key={character._id}>
-    <h3><UserOutlined /> Description: {character.description}</h3>
-    <h3><UserOutlined /> Universe: {character.universe}</h3>
-    <h3><UserOutlined /> Satus: {character.status}</h3>
+          <Col span={6} style={{margin: "20px"}}>
+          <Card title={character.name} bordered={false} style={{ textAlign: 'center', background:'#FFDAD1' }}>
+            <div key={character._id} style={{textAlign: "left"}}>
+              <h3>Description: {character.description}</h3>
+              <h3>Universe: {character.universe}</h3>
+              <h3>Satus: {character.status}</h3>
+            </div>
+          </Card>
+          </Col>
+        ))}
+        </Row>
     </div>
-</Card>
-
-
-      
-     
- 
-
-    
-
-   
-
-))
-
-}
-</div>
-    </div>
-    
   );
 }  
               
